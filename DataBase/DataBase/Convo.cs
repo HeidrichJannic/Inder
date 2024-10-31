571a93b6d6da9c03be7b4030d731419ac1e788f9
PatrikCer <patrik.cerna@plan-b-gmbh.com>
2024-10-31 11:02:18 +0100

    DataBase initial commit

---
+++ DataBase/DataBase/Convo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataBase
+{
+    public class Convo
+    {
+        public Convo(int matchID)
+        {
+            MatchID = matchID;
+        }
+
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Match")]
+        public int MatchID { get; set; }
+
+        public Match Match { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+
+}
---
+++ DataBase/DataBase/User.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataBase
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
+    using System.Text.RegularExpressions;
+
+    public class User
+    {
+        public User(string name, string surname, Gender gender, LookingFor lookingFor, byte[] profilePic)
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            LookingFor = lookingFor;
+            ProfilePic = profilePic;
+        }
+
+        [Key]
+        public int ID { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Surname { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        public int? Height { get; set; }
+        public int? Weight { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public Gender LookingFor { get; set; }
+
+        [MaxLength(500)]
+        public string? Bio { get; set; }
+
+        [Required]
+        public byte[] ProfilePic { get; set; }
+
+        // Navigation properties
+        public ICollection<Rate> RatesFromUser { get; set; }
+        public ICollection<Rate> RatesToUser { get; set; }
+        public ICollection<Match> MatchesAsFirstUser { get; set; }
+        public ICollection<Match> MatchesAsSecondUser { get; set; }
+    }
+
+    public enum Gender
+    {
+        Divers,
+        Male,
+        Female
+    }
+
+}
