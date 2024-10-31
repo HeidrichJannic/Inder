7a277d0ea5e22533c324975ec10ef474b09482a6
DavidImmanuelSchwieder <David.Schwieder@plan-b-gmbh.com>
2024-10-31 10:09:49 +0100

    Created Objects

--- BackEnd/Inder.Contracts/User/UserDTO.cs
+++ BackEnd/Inder.Contracts/User/UserDTO.cs
@@ -2,6 +2,24 @@
 {
     public class UserDTO : IUserDTO
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public int Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public string Gender { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Bio { get; set; }
+
+        public string ProfilePicture { get; set; }
     }
 }
