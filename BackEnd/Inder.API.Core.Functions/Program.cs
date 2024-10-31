01965159e5e545484f43bb15861d0298b55a879c
DavidS-PlanB <david.schaefer@plan-b-gmbh.com>
2024-10-31 13:27:11 +0100

    Automapper in der program.cs gefixxed

--- BackEnd/Inder.API.Core.Functions/Program.cs
+++ BackEnd/Inder.API.Core.Functions/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Inder.API.Core;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
-    {
+    {   
+        //AutoMapper
+        services.AddMapper(entity => entity.AddProfile<MapperService>(), AppDomain.CurrentDomain.GetAssemblies());
+
+        
+
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
+    
     .Build();
-
 host.Run();
