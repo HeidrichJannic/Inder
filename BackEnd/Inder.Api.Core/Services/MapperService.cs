887f66805f401ad0fb328b1512eef2eca5f1259b
DavidS-PlanB <david.schaefer@plan-b-gmbh.com>
2024-10-31 11:21:10 +0100

    Automapper updatet

--- BackEnd/Inder.Api.Core/Services/MapperService.cs
+++ BackEnd/Inder.Api.Core/Services/MapperService.cs
@@ -13,16 +13,15 @@ namespace Inder.Api.Core.Services
     {
         public MapperService()
         {
-            CreateMap<UserCreateDTO, UserModel>()           
+            CreateMap<UserCreateDTO, UserDTO>()           
             .ForMember(entity => entity.Name, source => source.MapFrom(source => source.Name))
             .ForMember(entity => entity.Surname, source => source.MapFrom(source => source.Surname))
             .ForMember(entity => entity.Age, source => source.MapFrom(source => source.Age))
             .ForMember(entity => entity.Height, source => source.MapFrom(source => source.Height))
             .ForMember(entity => entity.Weight, source => source.MapFrom(source => source.Weight))
             .ForMember(entity => entity.Gender, source => source.MapFrom(source => source.Gender))
-            .ForMember(entity => entity.ID, source => source.MapFrom(source => source.ID))
             .ForMember(entity => entity.Bio, source => source.MapFrom(source => source.Bio))
-            .ForMember(entity => entity.ProfilePic, source => source.MapFrom(source => source.ProfilePic));
+            .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePicture));
 
             CreateMap<UserDTO, UserModel>()
            .ForMember(entity => entity.ID, source => source.MapFrom(source => source.ID))
@@ -32,9 +31,9 @@ namespace Inder.Api.Core.Services
            .ForMember(entity => entity.Height, source => source.MapFrom(source => source.Height))
            .ForMember(entity => entity.Weight, source => source.MapFrom(source => source.Weight))
            .ForMember(entity => entity.Gender, source => source.MapFrom(source => source.Gender))
-           .ForMember(entity => entity.ID, source => source.MapFrom(source => source.ID))
+
            .ForMember(entity => entity.Bio, source => source.MapFrom(source => source.Bio))
-           .ForMember(entity => entity.ProfilePic, source => source.MapFrom(source => source.ProfilePic));
+           .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePicture));
 
 
             CreateMap<UserDeleteDTO, UserModel>()
@@ -48,9 +47,8 @@ namespace Inder.Api.Core.Services
            .ForMember(entity => entity.Height, source => source.MapFrom(source => source.Height))
            .ForMember(entity => entity.Weight, source => source.MapFrom(source => source.Weight))
            .ForMember(entity => entity.Gender, source => source.MapFrom(source => source.Gender))
-           .ForMember(entity => entity.ID, source => source.MapFrom(source => source.ID))
            .ForMember(entity => entity.Bio, source => source.MapFrom(source => source.Bio))
-           .ForMember(entity => entity.ProfilePic, source => source.MapFrom(source => source.ProfilePic));
+           .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePicture));
 
 
         }
