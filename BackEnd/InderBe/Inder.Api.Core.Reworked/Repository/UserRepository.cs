79d2ad2bdfff22fe60ec97ebec2d3078d18a0472
DavidImmanuelSchwieder <David.Schwieder@plan-b-gmbh.com>
2024-10-31 15:12:14 +0100

    Added new Solution

--- BackEnd/InderBe/Inder.Api.Core.Reworked/Repository/UserRepository.cs
+++ BackEnd/InderBe/Inder.Api.Core.Reworked/Repository/UserRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Inder.Contracts.User;
 using DataBase;
+using Newtonsoft.Json;
 
 namespace Inder.Api.Core.Repository;
 
--- BackEnd/InderBe/Inder.Api.Core.Reworked/Services/MapperService.cs
+++ BackEnd/InderBe/Inder.Api.Core.Reworked/Services/MapperService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using DataBase;
 using Inder.Contracts.User;
 //using verweise hinzufügen TODO
 
@@ -24,7 +25,7 @@ namespace Inder.Api.Core.Services
             .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePicture));
 
             CreateMap<UserDTO, UserModel>()
-           .ForMember(entity => entity.ID, source => source.MapFrom(source => source.ID))
+           .ForMember(entity => entity.ID, source => source.MapFrom(source => source.Id))
            .ForMember(entity => entity.Name, source => source.MapFrom(source => source.Name))
            .ForMember(entity => entity.Surname, source => source.MapFrom(source => source.Surname))
            .ForMember(entity => entity.Age, source => source.MapFrom(source => source.Age))
@@ -33,7 +34,7 @@ namespace Inder.Api.Core.Services
            .ForMember(entity => entity.Gender, source => source.MapFrom(source => source.Gender))
 
            .ForMember(entity => entity.Bio, source => source.MapFrom(source => source.Bio))
-           .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePicture));
+           .ForMember(entity => entity.ProfilePic, source => source.MapFrom(source => source.ProfilePicture));
 
            CreateMap<UserModel, UserDTO>()
            .ForMember(entity => entity.Id, source => source.MapFrom(source => source.ID))
@@ -44,7 +45,7 @@ namespace Inder.Api.Core.Services
            .ForMember(entity => entity.Weight, source => source.MapFrom(source => source.Weight))
            .ForMember(entity => entity.Gender, source => source.MapFrom(source => source.Gender))
            .ForMember(entity => entity.Bio, source => source.MapFrom(source => source.Bio))
-           .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePicture));
+           .ForMember(entity => entity.ProfilePicture, source => source.MapFrom(source => source.ProfilePic));
 
 
         }
